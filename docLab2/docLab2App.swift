1776d6caed10bcc1f0d20ccf853f68778fd6953e
Nadi13 <nadia_yudintseva@mail.ru>
2023-11-24 02:54:49 +0100

    firest commit

---
+++ docLab2/docLab2App.swift
@@ -0,0 +1,17 @@
+//
+//  docLab2App.swift
+//  docLab2
+//
+//  Created by user225687 on 11/24/23.
+//
+
+import SwiftUI
+
+@main
+struct docLab2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
